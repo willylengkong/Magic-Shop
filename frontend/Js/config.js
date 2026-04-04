809d1ff5c8f4738915124ecbcef617987632e17f
willylengkong <willylengkonggg@gmail.com>
2026-04-04 19:37:25 +0700

    Update Dasboard Admin

--- frontend/Js/config.js
+++ frontend/Js/config.js
@@ -3,5 +3,5 @@
  * Ganti BASE_URL jika backend berjalan di port atau host yang berbeda
  */
 const CONFIG = {
-  BASE_URL: "https:/mhfj210j-8000.asse.devtunnels.ms/",
+  BASE_URL: "https://mhfj210j-8000.asse.devtunnels.ms",
 };
